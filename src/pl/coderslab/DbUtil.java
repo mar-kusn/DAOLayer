cc7422533c8de5d5ffa0b06af4b5764de3edc261
Mariusz Kuśnierczyk <mar-kus@o2.pl>
2020-09-26 13:16:14 +0200

    DBUtil and User class created

---
+++ src/pl/coderslab/DbUtil.java
@@ -0,0 +1,19 @@
+package pl.coderslab;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static String DB_URL = "jdbc:mysql://localhost:3306/workshop2" +
+            "?useSSL=false" +
+            "&characterEncoding=utf8" +
+            "&serverTimezone=UTC";
+    private static String DB_USER = "root";
+    private static String DB_PASS = "coderslab";
+
+    private static final String DELETE_QUERY = "DELETE FROM tableName where id = ?";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+}
